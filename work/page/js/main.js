7b342b7afac396d389db7e314423f6afb46d26d4
Vago Aleksandr <vgoalex666@mail.ru>
2022-04-14 23:39:41 +0300

    feat: desktop version of this project has done, refactored files js

--- work/page/js/main.js
+++ work/page/js/main.js
@@ -1,18 +1,12 @@
-const UI = {
-	prevButton: document.querySelector('.btn-prev'),
-	nextButton: document.querySelector('.btn-next'),
-	slides: document.querySelectorAll('.slide'),
-	slideBeginners: document.querySelector('.slide--beginners'),
-	slideSpecialists: document.querySelector('.slide--specialists'),
-	slideLeaders: document.querySelector('.slide--leaders'),
-	dots: document.querySelectorAll('.dot'),
-	footerLogo: document.getElementById('logo-footer'),
-};
-
-console.log(UI.footerLogo);
+import { UI } from './UI.js';
+import { activeSlide, activeDot, activeFooter } from './actives.js';
 
 let index = 0;
 slideAndDotActive(index);
+
+setInterval(() => {
+	nextSlide();
+}, 5000);
 
 UI.nextButton.addEventListener('click', nextSlide);
 UI.prevButton.addEventListener('click', prevSlide);
@@ -41,69 +35,6 @@ function slideAndDotActive(numbIndex) {
 	activeSlide(numbIndex);
 	activeDot(numbIndex);
 	activeFooter(numbIndex);
-}
-
-function activeSlide(n) {
-	UI.slides.forEach(img => {
-		img.classList.remove('slide--active');
-	});
-	UI.slides[n].classList.add('slide--active');
-}
-
-function activeDot(n) {
-	const beginnersID = UI.dots[n].id == 'dot-beginners';
-	const specialistsID = UI.dots[n].id == 'dot-specialists';
-	const leadersID = UI.dots[n].id == 'dot-leaders';
-
-	const slidesClasses = {
-		beginners: 'dot--beginners',
-		specialists: 'dot--specialists',
-		leaders: 'dot--leaders',
-	};
-
-	UI.dots.forEach(dot => {
-		dot.classList.remove('dot--active');
-
-		dot.classList.remove(slidesClasses.beginners);
-		dot.classList.remove(slidesClasses.specialists);
-		dot.classList.remove(slidesClasses.leaders);
-	});
-
-	UI.dots.forEach(dot => {
-		dot.classList.remove('dot--active');
-	});
-
-	if (beginnersID) {
-		UI.dots[n].classList.add(slidesClasses.beginners);
-	} else if (specialistsID) {
-		UI.dots[n].classList.add(slidesClasses.specialists);
-	} else if (leadersID) {
-		UI.dots[n].classList.add(slidesClasses.leaders);
-	}
-}
-
-function activeFooter(n) {
-	const beginnersID = UI.dots[n].id == 'dot-beginners';
-	const specialistsID = UI.dots[n].id == 'dot-specialists';
-	const leadersID = UI.dots[n].id == 'dot-leaders';
-
-	const slidesClasses = {
-		beginners: 'logo--beginners',
-		specialists: 'logo--specialists',
-		leaders: 'logo--leaders',
-	};
-
-	UI.footerLogo.classList.remove(slidesClasses.beginners);
-	UI.footerLogo.classList.remove(slidesClasses.specialists);
-	UI.footerLogo.classList.remove(slidesClasses.leaders);
-
-	if (beginnersID) {
-		UI.footerLogo.classList.add(slidesClasses.beginners);
-	} else if (specialistsID) {
-		UI.footerLogo.classList.add(slidesClasses.specialists);
-	} else if (leadersID) {
-		UI.footerLogo.classList.add(slidesClasses.leaders);
-	}
 }
 
 UI.dots.forEach((el, dotIndex) => {
